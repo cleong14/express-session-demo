f6b8c364f7f342e79ddfc211e31f26e01c107c7e
Chaz Leong <cleong14@gmail.com>
2016-01-27 19:21:47 -1000

    added body-parser

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 var express = require('express');
 // =======1=======
 
+var bodyParser = require('body-parser');
+
 // =======2=======
 var PORT = 3000;
 // =======2=======
@@ -15,6 +17,8 @@ var app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 // basic page counter (5-6)
 // =======5=======
 var count = 0;
@@ -22,13 +26,21 @@ var count = 0;
 
 // =======6=======
 app.get('/', function (req, res) {
-  res.end('You are visitor: ' + count++);
+  count += 1;
+  res.end('You are visitor: ' + count);
 });
 // =======6=======
 
-// app.get('/name', function (req, res) {
+app.get('/name', function (req, res) {
+  // renders a view and sends rendered HTML string to client
+  // basically renders the page so that it can be dispalyed on the browser
+  res.render('get-name');
+});
 
-// });
+app.post('/name', function (req, res) {
+  // req.body.name - takes info from req, parses the info, and grabs the name info from the req
+  res.render('greet', {name: req.body.name});
+});
 
 // =======4=======
 var server = app.listen(PORT, function () {
